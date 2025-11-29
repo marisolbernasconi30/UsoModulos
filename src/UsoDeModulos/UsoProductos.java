4ad46d52499c4c4f0e0689a2b0d77e1063db0dbe
marisolbernasconi30 <usa2018sb1030@gmail.com>
2025-11-29 12:04:02 -0300

    adelantos

--- src/UsoDeModulos/UsoProductos.java
+++ src/UsoDeModulos/UsoProductos.java
@@ -1,5 +1,23 @@
 package UsoDeModulos;
-
+/*
+La idea es que la aplicacion arrance acá
+*/
 public class UsoProductos {
 
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a la aplicacion de gestion de productos");
+    }
 }
+
+
+
+/*
+
+PARA EJECUTAR DESDE CONSOLA:
+
+cd "/home/marisol/eclipse-workspace/PROYECTO/Modulos/UsoModulos/src/UsoDeModulos/"
+
+cd "/home/marisol/eclipse-workspace/PROYECTO/Modulos/UsoModulos/src/UsoDeModulos/" && javac UsoProductos.java && cd .. && java UsoDeModulos.UsoProductos
+
+
+*/
